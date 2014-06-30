e57280a763490745f71e8ea8a5e09ef0bfc8de46
Colin Woodcock <cwoodcock@netsrv-consulting.com>
2014-06-30 11:23:45 +0100

    berks scaffold

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'servicemix'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures servicemix'
+long_description 'Installs/Configures servicemix'
+version          '0.1.0'
+
